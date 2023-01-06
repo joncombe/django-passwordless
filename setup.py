68895a9e685bdea66aee9341e67e5935c9ef371d
Jon Combe <github@joncombe.net>
2023-01-06 20:59:29 +0700

    Improved messaging in emails

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="django-passwordless",
-    version="1.0.1",
+    version="1.0.2",
     author="Jon Combe",
     author_email="me@joncombe.net",
     packages=find_packages(),
